9bfd42f7c039fbd4e2c05713576a01ad05068d46
HERLIANSYAH Riki <s1898267@ed.ac.uk>
2021-01-18 19:33:07 +0000

    Update mtbh.R

--- mtbh.R
+++ mtbh.R
@@ -1,6 +1,5 @@
 #create a function for general Mh-type models
-mtbh <-function(x, method = 'LA2', model = 'Mh', npoints=50, maxit=1e+03, niter=5, 
-                starting.value = NULL){
+mtbh <-function(x, method = 'LA2', model = 'Mh', npoints=50, maxit=1000, niter=5){
   x <- as.matrix(x); n <- dim(x)[1]; T <- dim(x)[2]
   ys = rowSums(x);  ys = ys[ys!=0]
   nk = rep(0,T)
@@ -9,101 +8,46 @@ mtbh <-function(x, method = 'LA2', model = 'Mh', npoints=50, maxit=1e+03, niter=
   }
   y = matrix(0, nrow=n, ncol=T)
   for (i in 1:n) {
-    for (j in 1:(T-1)) {
-      if (x[i,j] == 1){
-        y[i,c((j+1):T)] = c(rep(1,T-j))
-        y[i,j] = 0
-        break
-      } else if(x[i,T] == 1){
-        y[i,T] = 0
-      } else {
-        y[i,j] = 0
+    fi = min(which(x[i,]==1))
+    if (fi == T) {
+      y[i,T] = 0
+    } else {
+      for (j in fi:(T-1)) {
+        y[i,(j+1)] = 1
       }
     }
   }
-  alpha <- NULL; 
+  
   w <- gauss.quad(npoints,"hermite")$weights
   node <- gauss.quad(npoints,"hermite")$nodes
-  if(model == 'Mh'){
-    y <- matrix(0, nrow = n, ncol = T)
-    if(method == 'LA2'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=1, model=1, niter=niter)
-      parameters <- list(alphat=rep(0,1), log_sigma=0, N=n)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    } 
-    else if (method == 'LA4'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=2, model=1, niter=niter)
-      parameters <- list(alphat=rep(0,1), log_sigma=0, N=n)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    }
-    else if (method == 'GHQ'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=3, model=1, niter=niter)
-      parameters <- list(alphat=rep(0,1), log_sigma=0, N=n)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    }
+  data <- list()
+  parameters <- list()
+  if(model == 'Mh' | model == 'Mbh'){
+    if (model == "Mh") {model = 1}
+    else {model = 3}
+    parameters <- list(alphat=rep(0,1), log_sigma=0, N=n, b=0)
+    if(method == 'LA2'){data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=1, model=model, niter=niter)} 
+    else if (method == 'LA4'){data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=2, model=model, niter=niter)}
+    else if (method == 'GHQ'){data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=3, model=model, niter=niter)}
     else { print('Select the correct method')}
   }
-  else if (model == 'Mth'){
-    y <- matrix(0, nrow = n, ncol = T)
-    if(method == 'LA2'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=1, model=2, niter=niter)
-      parameters <- list(alphat=rep(0,T), log_sigma=0, N=n)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    } 
-    else if (method == 'LA4'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=2, model=2, niter=niter)
-      parameters <- list(alphat=rep(0,T), log_sigma=0, N=n)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    }
-    else if (method == 'GHQ'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=3, model=2, niter=niter)
-      parameters <- list(alphat=rep(0,T), log_sigma=0, N=n)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    }
+  else if (model == 'Mth' | model == "Mtbh"){
+    if (model == "Mth") {model = 2}
+    else {model = 4}
+    parameters <- list(alphat=rep(0,T), log_sigma=0, N=n, b=0)
+    if(method == 'LA2'){data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=1, model=model, niter=niter)} 
+    else if (method == 'LA4'){data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=2, model=model, niter=niter)}
+    else if (method == 'GHQ'){data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=3, model=model, niter=niter)}
     else { print('Select the correct method')}
-  }
-  else if (model == 'Mbh'){
-    if(method == 'LA2'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=1, model=3, niter=niter)
-      parameters <- list(alphat=rep(0,1), log_sigma=0, N=n, b=0)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    } 
-    else if (method == 'LA4'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=2, model=3, niter=niter)
-      parameters <- list(alphat=rep(0,1), log_sigma=0, N=n, b=0)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    }
-    else if (method == 'GHQ'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=3, model=3, niter=niter)
-      parameters <- list(alphat=rep(0,1), log_sigma=0, N=n, b=0)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    }
-    else { print('Select the correct method')}
-  }
-  else if (model == 'Mtbh'){
-    if(method == 'LA2'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=1, model=4, niter=niter)
-      parameters <- list(alphat=rep(0,T), log_sigma=0, N=n, b=0)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    } 
-    else if (method == 'LA4'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=2, model=4, niter=niter)
-      parameters <- list(alphat=rep(0,T), log_sigma=0, N=n, b=0)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    }
-    else if (method == 'GHQ'){
-      data <- list(x=x, y=y, nk=nk, T=T, w=w, node=node, method=3, model=4, niter=niter)
-      parameters <- list(alphat=rep(0,T), log_sigma=0, N=n, b=0)
-      la.tmb <- MakeADFun(data, parameters, DLL='Mh_type', silent = TRUE)
-    }
-    else { print('Select the correct method')}
-  }
-  fit <- try(optim(la.tmb$par, la.tmb$fn, la.tmb$gr, la.tmb$he, method = 'BFGS', 
-               control = list(reltol=1e-8, maxit=maxit), hessian = FALSE), silent = TRUE)
+  } else {print('Select the correct model')}
+  
+  fittmb <- MakeADFun(data=data, parameters=parameters, DLL='Mh_type', silent = TRUE)
+  fit <- try(optim(fittmb$par, fittmb$fn, fittmb$gr, fittmb$he, method = 'BFGS', 
+                   control = list(reltol=1e-8, maxit=maxit), hessian = FALSE), silent = TRUE)
   k <- length(fit$par)
-  AIC <- 2*k + 2*la.tmb$fn(fit$par)
+  AIC <- 2*k + 2*fittmb$fn(fit$par)
   AICc <- AIC + ((2*k^2+2*k)/(n-k-1))
-  LogL <- data.frame(LogL = -la.tmb$fn(fit$par), AIC, AICc)
+  LogL <- data.frame(LogL = -fittmb$fn(fit$par), AIC, AICc)
   
   output <- list()
   output$par <- fit$par
