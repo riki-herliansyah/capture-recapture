01766b40f83826b645cd7f11fd9331122402c1c6
HERLIANSYAH Riki <s1898267@ed.ac.uk>
2021-01-18 19:33:51 +0000

    Update CJSc_MI.cpp

--- C++/CJSc_MI.cpp
+++ C++/CJSc_MI.cpp
@@ -5,9 +5,9 @@ Type objective_function<Type>::operator() ()
 {
   DATA_MATRIX(x);
   DATA_MATRIX(y);
-  DATA_IVECTOR(l);
-  DATA_IVECTOR(e);
-  DATA_IVECTOR(time_a);
+  DATA_IVECTOR(la);
+  DATA_IVECTOR(fi);
+  DATA_IVECTOR(timep);
   DATA_INTEGER(model);
 
   PARAMETER_VECTOR(beta);
@@ -28,11 +28,11 @@ Type objective_function<Type>::operator() ()
   matrix<Type> mu(n,T-1);
   matrix<Type> p(n,T-1);
 
-  if (model == 1){
+  if (model == 2 || model == 1){
   PARAMETER_VECTOR(alpha);
   vector<Type> alphat(T-1);
   for (int j = 0; j < T-1; j++){
-    alphat[j] = alpha(time_a[j]);
+    alphat[j] = alpha(timep[j]);
   }
   for (int i = 0; i < n; i++){
         for (int j = 0; j < T-1; j++){
@@ -42,7 +42,7 @@ Type objective_function<Type>::operator() ()
   }
   }
 
-  else if (model == 2){
+  else if (model == 4 || model == 3){
   PARAMETER_VECTOR(gamma);
   for (int i = 0; i < n; i++){
         for (int j = 0; j < T-1; j++){
@@ -56,18 +56,18 @@ Type objective_function<Type>::operator() ()
             chi[T-1] = 1;
             chi[T-j-1] = (1-phi(i,T-j-1)) + phi(i,T-j-1)*(1-p(i,T-j-1))*chi[T-j];
         }
-        if(e[i] == l[i]){
-            nll -= log(chi(l[i]-1));
+        if(fi[i] == la[i]){
+            nll -= log(chi(la[i]-1));
         } else {
             Type logp = 0.0;
-            for (int j = e[i]; j < l[i]; j++){
+            for (int j = fi[i]; j < la[i]; j++){
                 logp += x(i, j)*log(p(i,j-1)) + (1-x(i,j))*log(1-p(i,j-1));
             }
             Type logph = 0.0;
-            for (int j = (e[i]-1); j < (l[i]-1); j++){
+            for (int j = (fi[i]-1); j < (la[i]-1); j++){
                 logph += log(phi(i,j));
             }
-            nll -= logph + logp + log(chi(l[i]-1));
+            nll -= logph + logp + log(chi(la[i]-1));
         }
     }
     return nll;
